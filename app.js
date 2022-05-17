2ec53fb4096b01a2b3cc404ef08a58683ab3ed8f
sulaimondawod <sulaimondawod@gmail.com>
2022-05-17 15:51:29 +0100

    New changes

--- app.js
+++ app.js
@@ -12,4 +12,12 @@ btn.addEventListener('click', () => {
    
 })
 
-console.log(toggle);
+overlay.addEventListener('click', () => {
+    mobileLinks.classList.toggle('hidden');
+    mobileLinks.classList.toggle('show_links')
+    toggle.classList.toggle('show_links');
+    overlay.classList.toggle('hidden')
+    overlay.classList.toggle('show_overlay')
+})
+
+// console.log(toggle);
